d6d80e029cd8ca05b0a06be374eaa616c2e86ef9
sefunmi4 <sefunmiashiru0@gmail.com>
2025-06-08 14:42:47 -0400

    Add bitwise operator mapping example

--- runtime/wavefunction.h
+++ runtime/wavefunction.h
@@ -2,17 +2,36 @@
 #define QPP_WAVEFUNCTION_H
 
 #include <complex>
-#include <array>
+#include <vector>
+#include <cstddef>
 
 namespace qpp {
 class Wavefunction {
 public:
-    Wavefunction();
-    void apply_h();
-    void apply_x();
-    int measure() const;
-    std::array<std::complex<double>, 2> state;
+    explicit Wavefunction(std::size_t qubits = 1);
+
+    void apply_h(std::size_t qubit);
+    void apply_x(std::size_t qubit);
+    void apply_y(std::size_t qubit);
+    void apply_z(std::size_t qubit);
+    void apply_cnot(std::size_t control, std::size_t target);
+    void apply_cz(std::size_t control, std::size_t target);
+    void apply_ccnot(std::size_t c1, std::size_t c2, std::size_t target);
+    void apply_s(std::size_t qubit);
+    void apply_t(std::size_t qubit);
+    void apply_swap(std::size_t q1, std::size_t q2);
+
+    void reset();
+    std::complex<double> amplitude(std::size_t index) const;
+
+    int measure(std::size_t qubit);
+
+    std::vector<std::complex<double>> state;
+    std::size_t num_qubits;
 };
-}
+
+// TODO(good-first-issue): extend with parameterized rotations and register
+// import/export helpers
+} // namespace qpp
 
 #endif // QPP_WAVEFUNCTION_H
